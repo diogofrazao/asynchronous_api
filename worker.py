42bf3fdc4dc2f588d8e057b263f42635c694beab
diogo frazao <diogo.c.frazao@gmail.com>
2016-11-29 17:21:45 +0000

    Added function to kill worker

--- worker.py
+++ worker.py
@@ -4,12 +4,15 @@ import time
 class Worker(threading.Thread):
     def __init__(self, worker_id):
         self.worker_id = worker_id
-        self.signal = True
+        self._stop = threading.Event()
         super(Worker, self).__init__()
+
+    def stop(self):
+        self._stop.set()
 
     def run(self):
         i = 0
-        while self.signal:
+        while not self._stop.isSet():
             print "ID: "+str(self.worker_id)+ " Number: "+str(i)
             time.sleep(2)
             i = i + 1
